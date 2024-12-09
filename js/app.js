c9b6e6799066b6fccec3a7a12fd60a2897b0b75e
santiago <santiagobarliza6@gmail.com>
2024-12-09 09:17:14 -0500

    JS

---
+++ js/app.js
@@ -0,0 +1,12 @@
+alert ("Hola mundo!!!")
+
+// Un ejemplo de variable   
+//let nombre = "Ana";     //Se puede declarar variables con var y let
+//alert(nombre);
+
+let nombre = prompt ("Ingrese su nombre")       //Esta función que se usa para mostrar un cuadro de diálogo en el navegador que le permite al usuario ingresar información
+alert ("Hola " + nombre)
+
+nombre = prompt("Ingrese el segundo nombre")    //Esta función que se usa para mostrar un cuadro de diálogo en el navegador que le permite al usuario ingresar información
+alert ("Hola " + nombre)
+
